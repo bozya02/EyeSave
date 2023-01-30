88dee6e58b2d9270a2a2b9da91dcfd8a0e0dab9f
bozya02 <bozinandrey1@yandex.ru>
2023-01-30 09:33:22 +0300

    Add project files.

---
+++ EyeSave/DB/EyeSaveEntities.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EyeSave.DB
+{
+    public partial class EyeSaveEntities 
+    {
+        private static EyeSaveEntities _context;
+
+        public static EyeSaveEntities GetContext()
+        {
+            if (_context == null)
+                _context = new EyeSaveEntities();
+
+            return _context;
+        }
+    }
+}
---
+++ EyeSave/EyeSaveWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using EyeSave.Pages;
+
+namespace EyeSave
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class EyeSaveWindow : Window
+    {
+        public EyeSaveWindow()
+        {
+            InitializeComponent();
+            frame.NavigationService.Navigate(new AgentsListPage());
+            frame.Navigated += Frame_Navigated;
+        }
+
+        private void Frame_Navigated(object sender, NavigationEventArgs e)
+        {
+            var content = frame.Content as Page;
+
+            if (content is AgentsListPage)
+            {
+                btnBack.Visibility = Visibility.Collapsed;
+                btnForward.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                btnBack.Visibility = Visibility.Visible;
+                btnForward.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            if (frame.CanGoBack)
+                frame.GoBack();
+        }
+
+        private void btnForward_Click(object sender, RoutedEventArgs e)
+        {
+            if (frame.CanGoForward)
+                frame.GoForward();
+        }
+    }
+}
